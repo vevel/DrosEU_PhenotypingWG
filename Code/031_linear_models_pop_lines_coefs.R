13a484061530154e45b972a2f58235bdba4deb5d
vevel <envelkerdaffrec@gmail.com>
2025-03-25 21:54:25 +0100

    Cleaned up the file and ran the code. Results remain unchanged

--- Code/031_linear_models_pop_lines_coefs.R
+++ Code/031_linear_models_pop_lines_coefs.R
@@ -45,19 +45,19 @@ length(models)
 ### first get random effects with mixedup::extract_random_effects. This is a wrapper for lme4::ranef, gives deviations from each population intercept (not from the global intercept)
 
 line_re <- foreach(m = 1:length(models)) %do% {
-  e <- extract_random_effects(models[[m]], re = "Line:Population") %>% 
+  e <- extract_random_effects(models[[m]], re = "Line:Population") %>%
     separate(group, c("Line", "Population"), ":") %>%
     rename(Value_re = value, SE_re = se) %>%
-    dplyr::select(Population, Line, Value_re, SE_re) 
+    dplyr::select(Population, Line, Value_re, SE_re)
   info <- str_split(names(models)[m], "_") %>% unlist
   if (length(info) >= 5) info[1] <- paste(info[1], info[2], sep = "_")
   info[1] <- sub("_F", "", info[1])
   info[1] <- sub("_M", "", info[1])
-  e <- mutate(e, Model = paste(info[length(info)-1], info[length(info)], sep = "_"), 
-              Trait = info[1], Lab = info[length(info)-2], 
+  e <- mutate(e, Model = paste(info[length(info)-1], info[length(info)], sep = "_"),
+              Trait = info[1], Lab = info[length(info)-2],
               Sex = info[length(info)-3]) %>%
-    relocate(Model, Trait, Lab, Sex) 
-  if (!unique(e$Sex) %in% c("F", "M")) e$Sex <- "NA" 
+    relocate(Model, Trait, Lab, Sex)
+  if (!unique(e$Sex) %in% c("F", "M")) e$Sex <- "NA"
   if (length(unique(e$Trait)) == 1 & unique(e$Trait) %in% c("Dia", "Fec")) e$Sex <- "F"
   if (length(unique(e$Trait)) == 1 & grepl("Pgm", unique(e$Trait))) e$Sex <- "F"
   if (length(unique(e$Trait)) == 1 & grepl("LA_", unique(e$Trait))) e$Sex <- "B"
@@ -146,9 +146,3 @@ for (i in re){
   #trait_line_coefs_wide <- pivot_wider(trait_line_coefs, names_from = Lab, values_from = c(Coef, SE))
   #write.table(trait_line_coefs_wide, file = sub("_effects.rds", "_coefs_wide.txt", i), row.names = F, quote = F, sep = "\t")
 }
-
-
-   
-
-
-  
